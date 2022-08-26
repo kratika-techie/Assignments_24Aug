d721bd15805a1b24a7ed9082a4917f371977908a
kratika-techie <60233086+kratika-techie@users.noreply.github.com>
2022-08-26 23:57:38 +0530

    Create Assignment12.java

---
+++ Assignment12.java
@@ -0,0 +1,36 @@
+//Kratika's code
+
+import java.util.*;
+public class Assignment2 {
+	
+	static void characterCount(String str) {
+		HashMap<Character,Integer> hm=new HashMap<>();
+		
+		char[] strArray=str.toCharArray();
+		
+		for(char c:strArray) {
+			
+			if(hm.containsKey(c)) {
+				hm.put(c, hm.get(c)+1);
+				
+			}
+			else {
+				hm.put(c, 1);
+			}
+		}
+		for(Map.Entry entry:hm.entrySet()) {
+			
+			System.out.println(entry.getKey() + " " +entry.getValue());
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		
+		String str="Kratika";
+		characterCount(str);
+
+	}
+
+}
+

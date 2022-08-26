8cf9900c2a31921ed30e80865ec8012a58eaf6b4
kratika-techie <60233086+kratika-techie@users.noreply.github.com>
2022-08-27 00:01:16 +0530

    Add files via upload

---
+++ CharacterCount.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+        public class CharacterCount {
+
+        public static void characterCount(String inputString) {
+
+        HashMap<Character, Integer> charCountMap=new HashMap<Character, Integer>();
+
+        char[] charArray= inputString.toCharArray();
+
+        for(char ch: charArray) {
+        if (charCountMap.containsKey(ch)) {
+
+        charCountMap.put(ch, charCountMap.get(ch) + 1);
+     }
+
+             else {
+	charCountMap.put(ch, 1);
+    }
+    }
+
+        System.out.println(charCountMap);
+    }
+
+        public static void main(String args[]) {
+	String str = "Thisisasample";
+
+	characterCount(str);
+
+  }
+}
+
+
